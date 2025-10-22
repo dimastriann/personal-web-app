409ee3bc6766e071285c36f0e8e25d77fe2242a7
dimastriann <dimastriannugraha@gmail.com>
2025-10-22 10:45:42 +0700

    REF: remove unnecessary style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-// import "./AppStyle.css"
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
