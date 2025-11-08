901b8090ba0f062f619d6f88f853518363586318
dimastriann <dimastriannugraha@gmail.com>
2025-11-08 23:32:19 +0700

    delete unnecessary file and update vercel json

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
 //   output: 'export', // Outputs a Single-Page Application (SPA)
 //   distDir: 'build', // Changes the build output directory to `build`
-    allowedDevOrigins: ["127.0.0.1", "localhost"],
+  allowedDevOrigins: ["127.0.0.1", "localhost"],
 }
  
 export default nextConfig
